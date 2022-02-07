1a09dc8f592fd8a2f2cd383fd213016b97c142ca
iuliia <popova.iuliia.iu@gmail.com>
2022-02-07 14:55:26 +0300

    preparation.js >> index.js

--- src/index.js
+++ src/index.js
@@ -5,10 +5,31 @@ export const getRandom = (min, max) => {
   const maxNum = Math.floor(max);
   return Math.floor(Math.random() * (maxNum - minNum) + minNum);
 };
+
 export const getNameAndSayHello = (target) => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   console.log(target);
   return name;
+};
+
+export const game = (createLap, target) => {
+  const numberOfRounds = 3;
+  const name = getNameAndSayHello(target);
+
+  for (let i = 1; i <= numberOfRounds; i += 1) {
+    const questionAndTrueAnswer = createLap();
+    const goodAnswer = questionAndTrueAnswer[1];
+    const question = questionAndTrueAnswer[0];
+
+    console.log(question);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer.toString() !== goodAnswer.toString()) {
+      return console.log(`"${userAnswer}" is wrong answer;(. Correct answer was "${goodAnswer}".\nLet's try again, ${name}!`);
+    }
+    console.log('Correct!');
+  }
+  return console.log(`Congratulations, ${name}!`);
 };
