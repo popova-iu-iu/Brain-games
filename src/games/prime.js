7dce044026e436f1e4f737de563535c32317c8e5
iuliia <popova.iuliia.iu@gmail.com>
2022-02-10 21:23:20 +0300

    fixed error

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,12 +4,7 @@ import getRandom from '../utils.js';
 const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (number) => {
-  if (number === 0 || number === 1) {
-    return 'no';
-  }
-  const count = Math.sqrt(number);
-  const countRound = Math.round(count);
-  for (let i = 2; i < countRound;) {
+  for (let i = 2; i < number;) {
     if (number % i === 0) {
       return 'no';
     }
