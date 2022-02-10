7ea41a66d988173b5c3fdf7f56b5f91d33e67e6c
iuliia <popova.iuliia.iu@gmail.com>
2022-02-10 17:51:15 +0300

    getRandom moved to utils.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import readlineSync from 'readline-sync';
 
-export const getRandom = (min, max) => {
-  const minNum = Math.ceil(min);
-  const maxNum = Math.floor(max);
-  return Math.floor(Math.random() * (maxNum - minNum) + minNum);
-};
-
 export const getNameAndSayHello = (target) => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
