c8ca063950cc06068f8302167ca53680a73946af
iuliia <popova.iuliia.iu@gmail.com>
2022-02-10 17:59:21 +0300

    export > export default

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,7 @@
-export const getRandom = (min, max) => {
+const getRandom = (min, max) => {
   const minNum = Math.ceil(min);
   const maxNum = Math.floor(max);
   return Math.floor(Math.random() * (maxNum - minNum) + minNum);
 };
+
+export default getRandom;
