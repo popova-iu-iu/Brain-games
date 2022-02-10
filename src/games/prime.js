ba82a84c26a0555398070bf85442554f4a14aece
iuliia <popova.iuliia.iu@gmail.com>
2022-02-10 21:09:41 +0300

    fixed isPrime

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,6 +4,9 @@ import getRandom from '../utils.js';
 const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (number) => {
+  if (number === 0 || number === 1) {
+    return 'no';
+  }
   const count = Math.sqrt(number);
   const countRound = Math.round(count);
   for (let i = 2; i < countRound;) {
@@ -16,7 +19,7 @@ const isPrime = (number) => {
 };
 
 const generateRound = () => {
-  const randomNum = getRandom(1, 50);
+  const randomNum = getRandom(0, 50);
   const trueAnswer = isPrime(randomNum);
   const question = `Question: ${randomNum}`;
   return [trueAnswer, question];
